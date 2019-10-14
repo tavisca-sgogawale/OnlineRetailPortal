fe9aa6a6fe8fbd25fc837d61858b3ef309efcf7e
tavisca-sgogawale <sgogawale@tavisca.com>
2019-10-14 11:36:27 +0530

    Advertisement added.

---
+++ src/ERPBackend/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace ERPBackend.Models
+{
+    public enum Category
+    {
+      Properties,
+      Cars,
+      Furniture,
+      Mobiles,
+      Bikes,
+      Books,
+      Fashions,
+      Electronics
+    }
+}

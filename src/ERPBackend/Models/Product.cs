15134c717fa171ee5231ec29aae123c7dc9890c9
tavisca-sgogawale <sgogawale@tavisca.com>
2019-10-14 15:56:33 +0530

    Change class name advertisement to product

--- src/ERPBackend/Models/Product.cs
+++ src/ERPBackend/Models/Product.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace ERPBackend.Models
 {
-    public class Advertisement
+    public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }

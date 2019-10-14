d3734e40a6e4734200dd0f368036d8c37d3afcbb
tavisca-sgogawale <sgogawale@tavisca.com>
2019-10-14 15:56:33 +0530

    Change class name advertisement to product

---
+++ src/ERPBackend/Services/IAdvertisementProvider.cs
@@ -0,0 +1,9 @@
+﻿using ERPBackend.Models;
+using System;
+using System.Collections.Generic;
+
+public interface IProductProvider
+{
+    List<Product> GetProductsByPage(int pageNumber, int pageSize);
+    Product GetProductById(int Id);
+}

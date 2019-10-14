5b2fa55559ebf4990fdd067059a2d01930e114db
tavisca-sgogawale <sgogawale@tavisca.com>
2019-10-15 00:35:32 +0530

    Controller suport added

--- src/ERPBackend/Controllers/ProductsController.cs
+++ src/ERPBackend/Controllers/ProductsController.cs
@@ -2,14 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ERPBackend.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ERPBackend.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1.0/txs/[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly IProductProvider _productDb;
+
+        public ProductsController(MockProductDatabase mockProduct)
+        {
+            _productDb = mockProduct;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProductsByPage(int pageNumber,int pageSize) =>
+            _productDb.GetProductsByPage(pageNumber, pageNumber);
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProductById(int id)
+        {
+            var product = _productDb.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+
+        }
+            
     }
 }

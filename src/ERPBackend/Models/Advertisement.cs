fe813779cffb2492c33d8a93c006031fae60c47f
tavisca-sgogawale <sgogawale@tavisca.com>
2019-10-14 11:36:27 +0530

    Advertisement added.

---
+++ src/ERPBackend/Models/Advertisement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPBackend.Models
+{
+    public class Advertisement
+    {
+        public int Id { get; set; }
+        public Price Price { get; set; }
+        public Category Category { get; set; }
+        
+
+    }
+}
---
+++ src/ERPBackend/Models/Price.cs
@@ -0,0 +1,9 @@
+﻿namespace ERPBackend.Models
+{
+    public class Price
+    {
+        public int Amount { get; set; }
+        public bool IsNegotiable { get; set; }
+
+    }
+}

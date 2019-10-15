6383acde0843c01ba25437ac6ba3c2333ea04c41
tavisca-sgogawale <sgogawale@tavisca.com>
2019-10-15 08:44:55 +0530

    More products added

--- src/ERPBackend/Services/MockProductDatabase.cs
+++ src/ERPBackend/Services/MockProductDatabase.cs
@@ -11,14 +11,73 @@ public class MockProductDatabase : IProductProvider
                 HeroImage =new Image{Url = "https://www.olx.in/item/11-pro-max-64-gb-full-box-iid-1540782056/gallery"},
                 Description ="11 pro max 64 gb full box", Images=null,
                 PickupAddress =new Address{Line1="abc",Line2="xyz", City="Pune",State="Maharashtra", Pincode=411038 },
-                PostDateTime =  new DateTime(2019,12,1), PurchasedDate = new DateTime(2019,12,1), Status = Status.Active, UserId = "1118" },
+                PostDateTime =  new DateTime(2019,12,1), PurchasedDate = DateTime.Now, Status = Status.Active, UserId = "1118" },
 
             new Product{Id=102,Name="Bottle", Price=new Price{ Amount=999.00, IsNegotiable=true}, Category=Category.Others,
                 HeroImage =new Image{Url = "https://www.olx.in/item/11-pro-max-64-gb-full-box-iid-1540782056/gallery"},
                 Description ="Tavisca green color bottle", Images=null,
                 PickupAddress =new Address{Line1="abc",Line2="xyz", City="Pune",State="Maharashtra", Pincode=411038 },
-                PostDateTime =  new DateTime(2019,12,1), PurchasedDate =  new DateTime(2019,12,1), Status = Status.Active, UserId = "1118" }
+                PostDateTime =  new DateTime(2019,12,1), PurchasedDate =  new DateTime(2019,12,1), Status = Status.Active, UserId = "1112" },
 
+             new Product{Id=101,Name="Computer", Price=new Price{ Amount=11299.00, IsNegotiable=false}, Category=Category.Electronics,
+                HeroImage =new Image{Url = "https://www.olx.in/item/11-pro-max-64-gb-full-box-iid-1540782056/gallery"},
+                Description ="hp desktop", Images=null,
+                PickupAddress =new Address{Line1="abc",Line2="xyz", City="Pune",State="Maharashtra", Pincode=411038 },
+                PostDateTime =  new DateTime(2019,12,1), PurchasedDate = DateTime.Now, Status = Status.Active, UserId = "1128" },
+
+            new Product{Id=103,Name="Headphones", Price=new Price{ Amount=999.00, IsNegotiable=true}, Category=Category.Others,
+                HeroImage =new Image{Url = "https://www.olx.in/item/11-pro-max-64-gb-full-box-iid-1540782056/gallery"},
+                Description ="1 year used headphone", Images=null,
+                PickupAddress =new Address{Line1="abc",Line2="xyz", City="Pune",State="Maharashtra", Pincode=411038 },
+                PostDateTime =  new DateTime(2019,12,1), PurchasedDate =  new DateTime(2019,12,1), Status = Status.Active, UserId = "1748" },
+
+             new Product{Id=104,Name="Watch", Price=new Price{ Amount=1299.00, IsNegotiable=true}, Category=Category.Fashions,
+                HeroImage =new Image{Url = "https://www.olx.in/item/11-pro-max-64-gb-full-box-iid-1540782056/gallery"},
+                Description ="rolex watch", Images=null,
+                PickupAddress =new Address{Line1="abc",Line2="xyz", City="Pune",State="Maharashtra", Pincode=411038 },
+                PostDateTime =  new DateTime(2019,12,1), PurchasedDate = DateTime.Now, Status = Status.Active, UserId = "918" },
+
+            new Product{Id=105,Name="Car", Price=new Price{ Amount=93399.00, IsNegotiable=true}, Category=Category.Others,
+                HeroImage =new Image{Url = "https://www.olx.in/item/11-pro-max-64-gb-full-box-iid-1540782056/gallery"},
+                Description ="Old maruti 800", Images=null,
+                PickupAddress =new Address{Line1="abc",Line2="xyz", City="Pune",State="Maharashtra", Pincode=411038 },
+                PostDateTime =  new DateTime(2019,12,1), PurchasedDate =  new DateTime(2019,12,1), Status = Status.Active, UserId = "653" },
+
+             new Product{Id=106,Name="Bike", Price=new Price{ Amount=1299.00, IsNegotiable=true}, Category=Category.Bikes,
+                HeroImage =new Image{Url = "https://www.olx.in/item/11-pro-max-64-gb-full-box-iid-1540782056/gallery"},
+                Description ="4 year used R15", Images=null,
+                PickupAddress =new Address{Line1="abc",Line2="xyz", City="Pune",State="Maharashtra", Pincode=411038 },
+                PostDateTime =  new DateTime(2019,12,1), PurchasedDate = DateTime.Now, Status = Status.Active, UserId = "1768" },
+
+            new Product{Id=107,Name="Sofa", Price=new Price{ Amount=999.00, IsNegotiable=true}, Category=Category.Furniture,
+                HeroImage =new Image{Url = "https://www.olx.in/item/11-pro-max-64-gb-full-box-iid-1540782056/gallery"},
+                Description ="5 year old sofa", Images=null,
+                PickupAddress =new Address{Line1="abc",Line2="xyz", City="Pune",State="Maharashtra", Pincode=411038 },
+                PostDateTime =  new DateTime(2019,12,1), PurchasedDate =  new DateTime(2019,12,1), Status = Status.Active, UserId = "118" },
+
+             new Product{Id=108,Name="Bed", Price=new Price{ Amount=1299.00, IsNegotiable=true}, Category=Category.Furniture,
+                HeroImage =new Image{Url = "https://www.olx.in/item/11-pro-max-64-gb-full-box-iid-1540782056/gallery"},
+                Description ="1 year used bed", Images=null,
+                PickupAddress =new Address{Line1="abc",Line2="xyz", City="Pune",State="Maharashtra", Pincode=411038 },
+                PostDateTime =  new DateTime(2019,12,1), PurchasedDate = DateTime.Now, Status = Status.Active, UserId = "1118" },
+
+            new Product{Id=109,Name="Washing Mechine", Price=new Price{ Amount=999.00, IsNegotiable=true}, Category=Category.Electronics,
+                HeroImage =new Image{Url = "https://www.olx.in/item/11-pro-max-64-gb-full-box-iid-1540782056/gallery"},
+                Description ="LG washing mechine", Images=null,
+                PickupAddress =new Address{Line1="abc",Line2="xyz", City="Pune",State="Maharashtra", Pincode=411038 },
+                PostDateTime =  new DateTime(2019,12,1), PurchasedDate =  new DateTime(2019,12,1), Status = Status.Active, UserId = "138" },
+
+             new Product{Id=110,Name="TV", Price=new Price{ Amount=1299.00, IsNegotiable=true}, Category=Category.Electronics,
+                HeroImage =new Image{Url = "https://www.olx.in/item/11-pro-max-64-gb-full-box-iid-1540782056/gallery"},
+                Description ="Samsung 45' tv", Images=null,
+                PickupAddress =new Address{Line1="abc",Line2="xyz", City="Pune",State="Maharashtra", Pincode=411038 },
+                PostDateTime =  new DateTime(2019,12,1), PurchasedDate = DateTime.Now, Status = Status.Active, UserId = "875" },
+
+            new Product{Id=111,Name="Pendrive", Price=new Price{ Amount=999.00, IsNegotiable=true}, Category=Category.Others,
+                HeroImage =new Image{Url = "https://www.olx.in/item/11-pro-max-64-gb-full-box-iid-1540782056/gallery"},
+                Description ="Sandiscs 32 gb pen drive", Images=null,
+                PickupAddress =new Address{Line1="abc",Line2="xyz", City="Pune",State="Maharashtra", Pincode=411038 },
+                PostDateTime =  new DateTime(2019,12,1), PurchasedDate =  new DateTime(2019,12,1), Status = Status.Active, UserId = "1118" }
         };
     public Product GetProductById(int Id)
     {
@@ -27,6 +86,11 @@ public class MockProductDatabase : IProductProvider
     }
     public List<Product> GetProductsByPage(int pageNumber, int pageSize)
     {
+        _productList
+        .Select((x, i) => new { Index = i, pageSize = x })
+        .GroupBy(x => x.Index / 3)
+        .Select(x => x.Select(v => v.pageSize).ToList())
+        .ToList();
         return _productList;
     }
     public void AddProduct(Product product)

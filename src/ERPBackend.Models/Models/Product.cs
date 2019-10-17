9564e5dee19dac97a56f49b35eabddedacb9a941
tavisca-sgogawale <sgogawale@tavisca.com>
2019-10-17 08:51:58 +0530

    Models initialized

---
+++ src/ERPBackend.Models/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPBackend.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Price Price { get; set; }
+        public Category Category { get; set; }
+        public Image HeroImage { get; set; }
+        public Address PickupAddress { get; set; }
+        public List<Image> Images { get; set; }
+        public string UserId { get; set; }
+        public DateTime PostDateTime { get; set; }
+        public DateTime PurchasedDate { get; set; }
+        public string Description { get; set; }
+        public Status Status { get; set; }
+
+
+    }
+}
---
+++ src/ERPBackend.Models/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace ERPBackend.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string UserId { get; set; }
+        public string  EmailId { get; set; }
+        public int ContactNo { get; set; }
+        public bool IsContactVisible { get; set; }
+
+    }
+}

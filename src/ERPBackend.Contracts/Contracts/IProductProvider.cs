94724d5be62109e964892cf80022b4df41efaa0a
tavisca-sgogawale <sgogawale@tavisca.com>
2019-10-17 08:44:53 +0530

    Models library created

---
+++ src/ERPBackend.Contracts/Contracts/IProductProvider.cs
@@ -0,0 +1,9 @@
+﻿using ERPBackend.Models;
+using System.Collections.Generic;
+
+public interface IProductProvider
+{
+    List<Product> GetProductsByPage(int pageNumber, int pageSize);
+   // List<Product> GetProducts(int pageNumber, int pageSize);
+    Product GetProductById(int Id);
+}
--- src/ERPBackend/Models/Price.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ERPBackend.Models
-{
-    public class Price
-    {
-        public double Amount { get; set; }
-        public bool IsNegotiable { get; set; }
-
-    }
-}

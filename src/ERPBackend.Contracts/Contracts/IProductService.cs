05d8d2ccf3acfe0c3c5e980a4dc4a94843a95f48
Aditi Rupade <arupade@tavisca.com>
2019-10-17 10:13:29 +0530

    added interface in contract project

---
+++ src/ERPBackend.Contracts/Contracts/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using ERPBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPBackend.Contracts.Contracts
+{
+    public interface IProductService
+    {
+        Product AddProduct(Product product);
+    }
+}

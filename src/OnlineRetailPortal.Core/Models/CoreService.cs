9b0ac5eea5dd16c350c98f8ccbb8438fa9f7a920
Aditi Rupade <arupade@tavisca.com>
2019-10-23 16:01:07 +0530

    Added Translations for request response

--- src/OnlineRetailPortal.Core/Models/CoreService.cs
+++ src/OnlineRetailPortal.Core/Models/CoreService.cs
@@ -14,6 +14,7 @@ namespace OnlineRetailPortal.Core
 
         public CoreService(IProductStore productStore)
         {
+            
             this.productStore = productStore;
             translator = new Translator();
         }

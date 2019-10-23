4a1b68de04f069c6798902faf3a28d88eda4aef3
Aditi Rupade <arupade@tavisca.com>
2019-10-23 18:57:25 +0530

    Adjusted spacing

--- src/OnlineRetailPortal.Core/Translators/Translator.cs
+++ src/OnlineRetailPortal.Core/Translators/Translator.cs
@@ -9,16 +9,25 @@ namespace OnlineRetailPortal.Core
         public static EntityPostRequest ToEntityRequest(this CorePostRequest product)
         {
             EntityPostRequest  entityStoreRequest = new EntityPostRequest();
+
             entityStoreRequest.Id = product.Id;
+
             entityStoreRequest.Name = product.Name;
+
             entityStoreRequest.Description = product.Description;
+
             entityStoreRequest.HeroImage.Url = product.HeroImage.Url;
+
             entityStoreRequest.Price.Amount = product.Price.Amount;
             entityStoreRequest.Price.isNegotiable = product.Price.isNegotiable;
+
             entityStoreRequest.Category = (Contracts.Category) product.Category.GetHashCode();
+
             for (int index = 0; index < product.Images.Count; index++)
                 entityStoreRequest.Images[index].Url = product.Images[index].Url;
+
             entityStoreRequest.PurchasedDate = product.PurchasedDate;
+
             entityStoreRequest.PickupAddress.Line1 = product.PickupAddress.Line1;
             entityStoreRequest.PickupAddress.Line2 = product.PickupAddress.Line2;
             entityStoreRequest.PickupAddress.City = product.PickupAddress.City;
@@ -31,19 +40,31 @@ namespace OnlineRetailPortal.Core
         public static CorePostResponse ToProduct(this EntityPostResponse entityStoreResponse)
         {
             CorePostResponse product = new CorePostResponse();
+
             product.Id = entityStoreResponse.Id;
+
             product.Name = entityStoreResponse.Name;
+
             product.Description = entityStoreResponse.Description;
+
             product.HeroImage.Url = entityStoreResponse.HeroImage.Url;
+
             product.Price.Amount = entityStoreResponse.Price.Amount;
             product.Price.isNegotiable = entityStoreResponse.Price.isNegotiable;
+
             product.Category = (Category) entityStoreResponse.Category.GetHashCode();
+
             product.Status = (Status) entityStoreResponse.Status.GetHashCode();
+
             product.PostDateTime = entityStoreResponse.PostDateTime;
+
             product.ExpirationDate = entityStoreResponse.ExpirationDate;
+
             for (int index = 0; index < product.Images.Count; index++)
                 product.Images[index].Url = entityStoreResponse.Images[index].Url;
+
             product.PurchasedDate = entityStoreResponse.PurchasedDate;
+
             product.PickupAddress.Line1 = entityStoreResponse.PickupAddress.Line1;
             product.PickupAddress.Line2 = entityStoreResponse.PickupAddress.Line2;
             product.PickupAddress.City = entityStoreResponse.PickupAddress.City;

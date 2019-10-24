145d380a4d4ea013169668f1d563fc49b9dd624f
Narkhede <nnarkhede@internal.tavisca.com>
2019-10-24 14:54:06 +0530

    addded getproduct functions

--- src/OnlineRetailPortal.Contracts/Contracts/IProductService.cs
+++ src/OnlineRetailPortal.Contracts/Contracts/IProductService.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using OnlineRetailPortal.Contracts.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OnlineRetailPortal.Contracts
 {
-    interface IProductService
+    public interface IProductService
     {
         Task<GetProductServiceResponse> GetProductAsync(string productId);
         Task<GetProductsServiceResponse> GetProductsAsync(GetProductsServiceRequest request);

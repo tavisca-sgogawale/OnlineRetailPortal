12a575d67dcc9033848a93239760480b5cb9f775
tavisca-rpandey <rpandey@tavisca.com>
2019-10-24 19:01:55 +0530

    Added Responses to Imagewrite

---
+++ src/OnlineRetailPortal.Contracts/Models/ImageWriterSuccessResponse.cs
@@ -0,0 +1,14 @@
+﻿using OnlineRetailPortal.Contracts.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineRetailPortal.Contracts.Models
+{
+    public class ImageWriterSuccessResponse:IImageWriterResponse
+    {
+        public bool Success { get; set; }
+
+        public string Response { get; set; }
+    }
+}

0e043f54ed446da64cfd914c36fc2619ebb6f0e3
tavisca-rpandey <rpandey@tavisca.com>
2019-10-24 18:48:19 +0530

    Added Image Handler and Writer Interfaces

---
+++ src/OnlineRetailPortal.Contracts/Contracts/IImageHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineRetailPortal.Contracts.Contracts
+{
+    public interface IImageHandler
+    {
+        Task<UploadImageResponse> UploadImage(UploadImageRequest request);
+        /// <summary>
+        /// Takes a DeleteImageRequest object containing the name of the file to delete from the temporary folder
+        /// </summary>
+        /// <param name="imageUrl"></param>
+        void DeleteImage(DeleteImageRequest request);
+    }
+    
+}
---
+++ src/OnlineRetailPortal.Contracts/Contracts/IImageWriter.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineRetailPortal.Contracts.Contracts
+{
+    public interface IImageWriter
+    {
+        Task<IImageWriterResponse> UploadImage(IFormFile file);
+    }
+}

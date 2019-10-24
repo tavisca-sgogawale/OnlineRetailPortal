c5091aa26d24f45d68d9eb5975257b1b7e13c313
tavisca-sgogawale <sgogawale@tavisca.com>
2019-10-24 20:40:48 +0530

    Translator added

--- src/OnlineRetailPortal.Services/Translators/GetProductsServiceResponseTranslator.cs
+++ src/OnlineRetailPortal.Services/Translators/GetProductsServiceResponseTranslator.cs
@@ -11,9 +11,36 @@ namespace OnlineRetailPortal.Services
 
         public static GetProductsServiceResponse ToGetProductsContract(this List<Core.Product> getProductsResponse)
         {
-            GetProductsServiceResponse response = new GetProductsServiceResponse();
+            GetProductsServiceResponse response = new GetProductsServiceResponse()
+            {
+                Products = getProductsResponse.Select(x => new Product()
+                {
+                    Name = x.Name,
+                    Id = x.Id,
+                    HeroImage = new Contracts.Image() { Url = x.HeroImage.Url },
+                    ExpirationDate = x.ExpirationDate,
+                    PostDateTime = x.PostDateTime,
+                    Description = x.Description,
+                    Price = new Contracts.Price() { Amount = x.Price.Amount, IsNegotiable = x.Price.IsNegotiable },
+                    PurchasedDate = x.PurchasedDate,
+                    PickupAddress = new Contracts.Address()
+                    {
+                        City = x.PickupAddress.City,
+                        State = x.PickupAddress.State,
+                        Line1 = x.PickupAddress.Line1,
+                        Line2 = x.PickupAddress.Line2,
+                        Pincode = x.PickupAddress.Pincode
+                    },
+                    Images = x.Images.Select(y => new Contracts.Image
+                    {
+                        Url = y.Url
+                    }).ToList(),
+                    Status = (Contracts.Status)x.Status.GetHashCode(),
+                    Category = (Contracts.Category)x.Category.GetHashCode()
+                }).ToList()
+            };
             return response;
-           
+
         }
 
     }

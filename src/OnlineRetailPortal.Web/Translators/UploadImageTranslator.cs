558b74c39b38fdd10a4114723ce1cd92cc7971ce
tavisca-rpandey <rpandey@tavisca.com>
2019-10-24 19:25:17 +0530

    Added Controller and Translators

---
+++ src/OnlineRetailPortal.Web/Translators/UploadImageTranslator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineRetailPortal.Contracts.Contracts;
+using System.Threading.Tasks;
+
+namespace OnlineRetailPortal.Web.Translators
+{
+    public static class UploadImageTranslator
+    {
+        public static UploadImageRequest ToUploadImageContract(this IFormFile file)
+        {
+            var request = new UploadImageRequest() { File = file};
+            return request;
+        }
+
+        public static IActionResult ToUser(this UploadImageResponse response)
+        {
+            if (response.Success)
+            {
+                return new OkObjectResult(response);
+            }
+            else
+            {
+                return new BadRequestObjectResult(response);
+
+            }
+
+
+        }
+    }
+}

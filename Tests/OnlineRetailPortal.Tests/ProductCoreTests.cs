b685fb87921b2c680d143ac5396c7c0900dadd4a
Aditi Rupade <arupade@tavisca.com>
2019-10-25 11:34:03 +0530

    Changed test case to check for each property

--- Tests/OnlineRetailPortal.Tests/ProductCoreTests.cs
+++ Tests/OnlineRetailPortal.Tests/ProductCoreTests.cs
@@ -20,7 +20,25 @@ namespace OnlineRetailPortal.Tests
 
             Core.Product actualResponse = await requestProduct.AddProductAsync(requestProduct);
 
-            Assert.Equal(expectedResponse.ToString(), actualResponse.ToString());
+            Assert.Equal(expectedResponse.Id, actualResponse.Id);
+            Assert.Equal(expectedResponse.Name, actualResponse.Name);
+            Assert.Equal(expectedResponse.Description, actualResponse.Description);
+            Assert.Equal(expectedResponse.HeroImage.Url,actualResponse.HeroImage.Url);
+            Assert.Equal(expectedResponse.Price.Amount, actualResponse.Price.Amount);
+            Assert.Equal(expectedResponse.Price.IsNegotiable, actualResponse.Price.IsNegotiable);
+            Assert.Equal(expectedResponse.Category, actualResponse.Category);
+            Assert.Equal(expectedResponse.Status, actualResponse.Status);
+            Assert.Equal(expectedResponse.PostDateTime.ToString(), actualResponse.PostDateTime.ToString());
+            Assert.Equal(expectedResponse.ExpirationDate.ToString(), actualResponse.ExpirationDate.ToString());
+            for(var i=0; i<actualResponse.Images.Count;i++)
+                Assert.Equal(expectedResponse.Images[i].Url, actualResponse.Images[i].Url);
+            Assert.Equal(expectedResponse.PurchasedDate.ToString(), actualResponse.PurchasedDate.ToString());
+            Assert.Equal(expectedResponse.PickupAddress.Line1, actualResponse.PickupAddress.Line1);
+            Assert.Equal(expectedResponse.PickupAddress.Line2, actualResponse.PickupAddress.Line2);
+            Assert.Equal(expectedResponse.PickupAddress.City, actualResponse.PickupAddress.City);
+            Assert.Equal(expectedResponse.PickupAddress.State, actualResponse.PickupAddress.State);
+            Assert.Equal(expectedResponse.PickupAddress.Pincode, actualResponse.PickupAddress.Pincode);
+
         }
 
         private Core.Product GetExpectedResponse()

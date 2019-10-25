56b9dd224196c93596c6e727b5593cf54719e40c
Aditi Rupade <arupade@tavisca.com>
2019-10-25 10:20:36 +0530

    Added null checks in translation

--- src/OnlineRetailPortal.Core/Translators/Translator.cs
+++ src/OnlineRetailPortal.Core/Translators/Translator.cs
@@ -1,6 +1,7 @@
 ﻿using OnlineRetailPortal.Contracts;
 using OnlineRetailPortal.Core;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace OnlineRetailPortal.Core
@@ -9,55 +10,78 @@ namespace OnlineRetailPortal.Core
     {
         public static AddProductStoreRequest ToEntityRequest(this Product product)
         {
-            var  entityStoreRequest = new AddProductStoreRequest() {
+            if (product == null)
+                return null;
+            var  addProductStoreRequest = new AddProductStoreRequest() {
                 Id = product.Id,
                 Name = product.Name,
                 Description = product.Description,
                 HeroImage = new Contracts.Image { Url = product.HeroImage.Url},
                 Price = new Contracts.Price { Amount = product.Price.Amount, IsNegotiable = product.Price.IsNegotiable},
                 Category = (Contracts.Category)product.Category,
-                Images = product.Images.Select(x => new Contracts.Image
-                {
-                    Url = x.Url
-                }).ToList(),
-                PurchasedDate = product.PurchasedDate,
-                PickupAddress = new Contracts.Address
-                {
-                    Line1 = product.PickupAddress.Line1,
-                    Line2 = product.PickupAddress.Line2,
-                    City = product.PickupAddress.City,
-                    Pincode = product.PickupAddress.Pincode,
-                    State = product.PickupAddress.State
-        }
+                Images = GetImages(product.Images),
+                PurchasedDate = GetPurchasedDates(product.PurchasedDate),
+                PickupAddress = GetAddress(product.PickupAddress)
             };
 
-            return entityStoreRequest;
+            return addProductStoreRequest;
         }
 
-        public static Product ToProduct(this AddProductStoreResponse entityStoreResponse)
+        private static Contracts.Address GetAddress(Address pickupAddress)
+        {
+            if (pickupAddress == null)
+                return null;
+            return new Contracts.Product().PickupAddress = new Contracts.Address()
+            {
+                Line1 = pickupAddress.Line1,
+                Line2 = pickupAddress.Line2,
+                City = pickupAddress.City,
+                Pincode = pickupAddress.Pincode,
+                State = pickupAddress.State
+            };
+        }
+
+        private static DateTime? GetPurchasedDates(DateTime? purchasedDate)
+        {
+            if (purchasedDate == null)
+                return null;
+            return new Contracts.Product().PurchasedDate = purchasedDate;
+        }
+
+        private static List<Contracts.Image> GetImages(List<Image> images)
+        {
+            if (images == null)
+                return null;
+            return new Contracts.Product().Images = images.Select(x => new Image
+            {
+                Url = x.Url
+            }).ToList();
+        }
+
+        public static Product ToProduct(this AddProductStoreResponse addProductStoreResponse)
         {
             var product = new Product() {
-                Id = entityStoreResponse.Id,
-                Name = entityStoreResponse.Name,
-                Description = entityStoreResponse.Description,
-                HeroImage = new Image { Url = entityStoreResponse.HeroImage.Url },
-                Price = new Price { Amount = entityStoreResponse.Price.Amount, IsNegotiable = entityStoreResponse.Price.IsNegotiable },
-                Category = (Category)entityStoreResponse.Category,
-                Status = (Status)entityStoreResponse.Status,
-                PostDateTime = entityStoreResponse.PostDateTime,
-                ExpirationDate = entityStoreResponse.ExpirationDate,
-                Images = entityStoreResponse.Images.Select(x => new Image
+                Id = addProductStoreResponse.Id,
+                Name = addProductStoreResponse.Name,
+                Description = addProductStoreResponse.Description,
+                HeroImage = new Image { Url = addProductStoreResponse.HeroImage.Url },
+                Price = new Price { Amount = addProductStoreResponse.Price.Amount, IsNegotiable = addProductStoreResponse.Price.IsNegotiable },
+                Category = (Category)addProductStoreResponse.Category,
+                Status = (Status)addProductStoreResponse.Status,
+                PostDateTime = addProductStoreResponse.PostDateTime,
+                ExpirationDate = addProductStoreResponse.ExpirationDate,
+                Images = addProductStoreResponse.Images.Select(x => new Image
                 {
                     Url = x.Url
                 }).ToList(),
-                PurchasedDate = entityStoreResponse.PurchasedDate,
+                PurchasedDate = addProductStoreResponse.PurchasedDate,
                 PickupAddress = new Address
                 {
-                    Line1 = entityStoreResponse.PickupAddress.Line1,
-                    Line2 = entityStoreResponse.PickupAddress.Line2,
-                    City = entityStoreResponse.PickupAddress.City,
-                    Pincode = entityStoreResponse.PickupAddress.Pincode,
-                    State = entityStoreResponse.PickupAddress.State
+                    Line1 = addProductStoreResponse.PickupAddress.Line1,
+                    Line2 = addProductStoreResponse.PickupAddress.Line2,
+                    City = addProductStoreResponse.PickupAddress.City,
+                    Pincode = addProductStoreResponse.PickupAddress.Pincode,
+                    State = addProductStoreResponse.PickupAddress.State
                 }
             };
 

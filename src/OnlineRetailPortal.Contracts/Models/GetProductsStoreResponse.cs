9dfa1c135a5a3b008e228af2b6859c852124e358
Aditi Rupade <arupade@tavisca.com>
2019-10-26 11:07:08 +0530

    Added mocking support for buyer side

---
+++ src/OnlineRetailPortal.Contracts/Models/GetProductsStoreResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineRetailPortal.Contracts
+{
+    public class GetProductsStoreResponse
+    {
+        public List<Product> Products { get; set; }
+    }
+}

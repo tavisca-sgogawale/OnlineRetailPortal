df5c70803b8daca3fa5bcefc6f30eb3ba147095d
Abhishek Charpe <acharpe@affinionds.com>
2019-11-06 11:50:56 +0530

    added Middleware

---
+++ src/OnlineRetailPortal.Web/Validations/EnsureValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FluentValidation.Results;
+using FluentValidation;
+using System.Linq;
+using OnlineRetailPortal.Contracts;
+using OnlineRetailPortal.Core;
+using System.Net;
+using System.Collections.Generic;
+
+namespace OnlineRetailPortal.Web.Validations
+{
+    public static class EnsureValidator
+    {
+        
+        public static void EnsureValid<AddProductRequest>(this AbstractValidator<AddProductRequest> validator, AddProductRequest request)
+        {
+            List<ErrorInfo> info = new List<ErrorInfo>();
+            var validationResult = validator.Validate(request);
+
+            if (request == null)
+                throw new BaseException(Convert.ToInt32(ErrorCode.NullRequest()), Error.NullRequest(), null, HttpStatusCode.BadRequest);
+
+            if (validationResult.IsValid == false)
+            {
+                foreach (var error in validationResult.Errors)
+                {
+                    info.Add(new ErrorInfo { Code = error.ErrorCode, Message = error.ErrorMessage });
+                }
+                throw new BaseException(Convert.ToInt32(ErrorCode.InvalidRequest()), Error.InvalidRequest(), info , HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}

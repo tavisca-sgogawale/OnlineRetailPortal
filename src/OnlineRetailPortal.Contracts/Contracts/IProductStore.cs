787c238bb56e61723d509ebef876f3d4c8150313
tavisca-cpadole <51901832+tavisca-cpadole@users.noreply.github.com>
2019-11-06 14:18:27 +0530

    Added MongoDB Store Class Library, interface implementations and models

--- src/OnlineRetailPortal.Contracts/Contracts/IProductStore.cs
+++ src/OnlineRetailPortal.Contracts/Contracts/IProductStore.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿using System.Threading.Tasks;
 namespace OnlineRetailPortal.Contracts
 {
     public interface IProductStore
     {
+        Task<AddProductStoreResponse> AddProductAsync(AddProductStoreRequest request);
+        Task<GetProductStoreResponse> GetProductAsync(GetProductStoreRequest request);
+        Task<GetProductsStoreResponse> GetProductsAsync(GetProductsStoreRequest request);
     }
 }

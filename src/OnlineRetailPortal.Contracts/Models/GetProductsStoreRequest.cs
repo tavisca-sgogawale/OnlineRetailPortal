6d6b98ef18a6dd9c28bd1ecef7a5fbdb4ac27034
tavisca-cpadole <51901832+tavisca-cpadole@users.noreply.github.com>
2019-11-06 14:18:27 +0530

    Added MongoDB Store Class Library, interface implementations and models

---
+++ src/OnlineRetailPortal.Contracts/Models/GetProductsStoreRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineRetailPortal.Contracts
+{
+    public class GetProductsStoreRequest
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public PageSortBy SortBy { get; set; }
+    }
+}

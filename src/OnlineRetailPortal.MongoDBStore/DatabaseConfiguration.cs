9a39d16774bd1e16321f920a979789539dee264a
Raunak <rsingh4work@gmail.com>
2019-11-07 14:41:22 +0530

    Added Server down timeout customization

--- src/OnlineRetailPortal.MongoDBStore/DatabaseConfiguration.cs
+++ src/OnlineRetailPortal.MongoDBStore/DatabaseConfiguration.cs
@@ -1,9 +1,19 @@
-﻿namespace OnlineRetailPortal.MongoDBStore
+﻿using MongoDB.Driver;
+using System;
+
+namespace OnlineRetailPortal.MongoDBStore
 {
     public static class DatabaseConfiguration
     {
         public static readonly string _mongoDBConnectionString = "mongodb://127.0.0.1:27017";
         public static readonly string _databaseName = "ORP";
         public static readonly string _productTableName = "Product";
-    }
+        public static MongoClientSettings _mongoSettings = new MongoClientSettings
+        {
+            Server = new MongoServerAddress(DatabaseConfiguration._mongoDBConnectionString),
+            SocketTimeout = new TimeSpan(0, 0, 0, 2),
+            WaitQueueTimeout = new TimeSpan(0, 0, 0, 2),
+            ConnectTimeout = new TimeSpan(0, 0, 0, 2)
+        };
+}
 }

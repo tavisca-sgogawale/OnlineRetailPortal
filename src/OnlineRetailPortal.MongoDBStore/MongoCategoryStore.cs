d96d0a0b7d5d463d6a278fcdbb027d796811c7d2
rarai <rarai@RENTLAPT126>
2019-11-20 11:16:29 +0530

    pulling new changes

---
+++ src/OnlineRetailPortal.MongoDBStore/MongoCategoryStore.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineRetailPortal.MongoDBStore
+{
+    class MongoCategoryStore
+    {
+        private static MongoClient _dbClient = new MongoClient(new MongoClientSettings
+        {
+            Server = new MongoServerAddress(MongoDBConfiguration.Url, int.Parse(MongoDBConfiguration.Port)),
+            SocketTimeout = new TimeSpan(0, 0, 0, 10),
+            WaitQueueTimeout = new TimeSpan(0, 0, 0, 2),
+            ConnectTimeout = new TimeSpan(0, 0, 0, 10)
+        });
+
+        private IMongoDatabase _db = _dbClient.GetDatabase(MongoDBConfiguration.Database);
+
+
+    }
+}

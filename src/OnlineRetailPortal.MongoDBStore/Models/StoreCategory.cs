88da4a134c427bc3efbfec0476baeae077f1c0b1
tavisca-cpadole <51901832+tavisca-cpadole@users.noreply.github.com>
2019-11-25 11:17:59 +0530

    Updated code

--- src/OnlineRetailPortal.MongoDBStore/Models/StoreCategory.cs
+++ src/OnlineRetailPortal.MongoDBStore/Models/StoreCategory.cs
@@ -1,13 +1,12 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
 
 namespace OnlineRetailPortal.MongoDBStore
 {
-    [BsonIgnoreExtraElements]
     public class StoreCategory
     {
-        [MongoDB.Bson.Serialization.Attributes.BsonId]
+        public ObjectId Id { get; set; }
         public string Name { get; set; }
         public List<string> Tags { get; set; }
         public DateTime CreatedDate { get; set; }
